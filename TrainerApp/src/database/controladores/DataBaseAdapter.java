b3d04edd0a7c701a1fd4e6bee1b683743ebff971
Hernan <hernano.f@gmail.com>
2014-06-17 00:46:47 -0400

    Commint final dia lunes

--- TrainerApp/src/database/controladores/DataBaseAdapter.java
+++ TrainerApp/src/database/controladores/DataBaseAdapter.java
@@ -4,7 +4,6 @@ import clases.dominio.DtoRutina;
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 import database.controladores.DataBaseHelper;
 
 public class DataBaseAdapter {
@@ -12,7 +11,7 @@ public class DataBaseAdapter {
     private SQLiteDatabase db;
     private final Context contexto;
     
-    public BaseDataAdapter(Context contexto) {
+    public DataBaseAdapter(Context contexto) {
             this.contexto = contexto;
     }
     public SQLiteDatabase open() throws SQLException {
@@ -24,13 +23,10 @@ public class DataBaseAdapter {
     	dbHelper.close();
     }
 	public boolean insertarRutina(DtoRutina Rutina ) {
-		   Log.w("INSERTAR RUTINA", "insert into "+ DataBaseHelper.TABLA_RUT+ " ( " + DataBaseHelper.ID_RUT+", "+DataBaseHelper.TIPO_TRAB 
-	        		+"," + DataBaseHelper.NIVEL + ","+DataBaseHelper.SEXO+") "+" values ('" +Rutina.ID_RUT +"','" +Rutina.TIPO_TRAB+"','"
-				   +Rutina.NIVEL +"','"+Rutina.SEXO+"')");
 	       try{
 	        db.execSQL("insert into "+ DataBaseHelper.TABLA_RUT+ " ( " + DataBaseHelper.ID_RUT+", "+DataBaseHelper.TIPO_TRAB 
-	        		+"," + DataBaseHelper.NIVEL + ","+DataBaseHelper.SEXO+") "+" values ('" +Rutina.ID_RUT +"','" +Rutina.TIPO_TRAB+"','"
-				   +Rutina.NIVEL+"','"+Rutina.SEXO+"')");
+	        		+"," + DataBaseHelper.NIVEL + ","+DataBaseHelper.SEXO+") "+" values ('" +Rutina.getIdRutina() +"','" +Rutina.getTipoTrabajo()+"','"
+				   +Rutina.getNivel()+"','"+Rutina.getSexo()+"')");
 	        return true;
 	       }catch(Exception e){return false;}
 	}
