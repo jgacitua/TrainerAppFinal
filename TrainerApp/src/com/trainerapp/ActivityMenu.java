0c668779f4850279cbbb3e436327dd9cfb52c08f
Jorge <Jorge@Mohami>
2014-06-19 12:43:12 +0200

    pocos cambios

--- TrainerApp/src/com/trainerapp/ActivityMenu.java
+++ TrainerApp/src/com/trainerapp/ActivityMenu.java
@@ -1,7 +1,5 @@
 package com.trainerapp;
 
-
-
 import interfaces.dialogos.DialogCustom;
 import interfaces.dialogos.DialogMaker;
 import database.controladores.DataBaseAdapter;
@@ -15,29 +13,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-
 public class ActivityMenu extends ActionBarActivity {
 	private DataBaseAdapter dbRutina;
     @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, new PlaceholderFragment())
+//                    .commit();
+//        }
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_menu, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -49,22 +42,9 @@ public class ActivityMenu extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_activity_menu, container, false);
-            return rootView;
-        }
-    }
     public void OnClickCrearEntrenamiento(View view)
     {       dbRutina = new DataBaseAdapter(this);
            dbRutina.open();
@@ -77,24 +57,20 @@ public class ActivityMenu extends ActionBarActivity {
     					dialog.dismiss();
     					Intent i = new Intent(ActivityMenu.this,ActivityCrearEntrenamiento.class);
     	    	    	startActivity(i);
-    					
     					}	
     			   });
     	    	dialog.setNegativeButton("Cancelar", new OnClickListener() {
-    	            
     	            @Override
     	            public void onClick(View v) {
     	            	dialog.dismiss();              
     	            }
     	        });
     		     dialog.show();
-    	    	
     	    }else{
     	    	dbRutina.close();
     	    	Intent i = new Intent(this,ActivityCrearEntrenamiento.class);
     	    	startActivity(i);
     	    }
-    	    
     }
     public void OnClickVerRutina(View view)
     {
